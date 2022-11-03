f06e815e5832e27fbe47536faaf459a1e2465feb
movie42 <movie3342@gmail.com>
2022-11-03 16:52:18 +0900

    feat: useChangeDetailData 구현
    디테일 페이지에서 쓰이는 데이터를 쓰기 쉽게 object로 변경합니다.
    return
    - detail :{
        id : 데이터 id,
        imageUrl : 데이터 이미지 주소,
        name : 상품 이름,
        brand : 브랜드 이름,
        amount: 상품 가격 ,
        segmentKorean : segment "전체, 소형, 중형, 대형, SUV",
        fuelKorean : fuel "가솔린, 전기, 하이브리드",
        insurance : {name: 보험 이름, description: 보험 세부 정보 }[],
        carInfoList : {name: 상품 세부 정보 이름, description: 상품 세부 정보 묘사}[],
        additionalProducts:{name: 추가 상품 이름, amount: 추가상품 가격(로컬스트링)}[]
    }

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useCalculateDate } from "./useCalculateDate";
 export { default as useChangeAmountToLocalString } from "./useChangeAmountToLocalString";
 export { default as useChangeFuelSegmentEnumToKorean } from "./useChangeFuelSegmentEnumToKorean";
+export { default as useChangeDetailData } from "./useChangeDetailData";
